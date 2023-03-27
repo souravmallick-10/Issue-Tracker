0e945bb4311c0c688884ae90daff04b913950d0c
Sourav Mallick <souravmallicks286@gmail.com>
2023-03-28 05:06:46 +0530

    first commit

---
+++ config/mongoose.js
@@ -0,0 +1,17 @@
+require('dotenv').config();
+//require the library
+const mongoose = require('mongoose');
+
+//connect to the database
+mongoose.connect("mongodb://localhost:27017/Issue-Tracker");
+
+// acquire connection (to check if its successful)
+const db = mongoose.connection;
+
+db.on('error', console.error.bind(console, 'Error connecting to MongoDB'));
+
+db.once('open', function () {
+  console.log('Connected to Database :: MongoDB');
+});
+
+module.exports = db;
